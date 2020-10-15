29bdf2ab5395b88b508f2ff9b9713d095517bd6e
viveloper <ucsik1234@gmail.com>
2020-10-15 20:12:59 +0900

    updated create product api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -58,16 +58,26 @@ const deleteProduct = asyncHandler(async (req, res) => {
 // @route   POST /api/products
 // @access  Private/Admin
 const createProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body
+
   const product = new Product({
-    name: 'Sample name',
-    price: 0,
+    name,
+    price,
     user: req.user._id,
-    image: '/images/sample.jpg',
-    brand: 'Sample brand',
-    category: 'Sample category',
-    countInStock: 0,
+    image,
+    brand,
+    category,
+    countInStock,
     numReviews: 0,
-    description: 'Sample description',
+    description,
   })
 
   const createdProduct = await product.save()
